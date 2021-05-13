734a0dabe0b9228811f05a922cb8fc47b9b954c4
Administrator <orelcokolov@gmail.com>
2021-05-13 21:24:12 +0300

    Add field spec

---
+++ spec/character_spec.rb
@@ -0,0 +1,11 @@
+describe Character do
+
+  context "Default" do
+
+  end
+
+  context "Modified" do
+
+  end
+
+end
--- spec/field_spec.rb
+++ spec/field_spec.rb
@@ -1,2 +1,39 @@
 describe Field do
+
+  context "Default 1x1" do
+    it "should allow position 0, 0" do
+      expect(subject.position_allowed?(0, 0)).to be_truthy
+    end
+
+    it "should disallow negative position" do
+      expect(subject.position_allowed?(-1, -1)).to be_falsey
+    end
+
+    it "should disallow position 1, 1" do
+      expect(subject.position_allowed?(1, 1)).to be_falsey
+    end
+
+    it "should disallow position 1, 0" do
+      expect(subject.position_allowed?(1, 0)).to be_falsey
+    end
+
+  end
+
+  context "5x5" do
+
+    subject {Field.new(5, 5)}
+
+    it "should allow position 4, 4" do
+      expect(subject.position_allowed?(4, 4)).to be_truthy
+    end
+
+    it "should disallow negative position" do
+      expect(subject.position_allowed?(-1, -1)).to be_falsey
+    end
+
+    it "should allow position 10, 4" do
+      expect(subject.position_allowed?(10, 4)).to be_falsey
+    end
+
+  end
 end
